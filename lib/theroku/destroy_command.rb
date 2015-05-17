ddb0a16a3cbe2acbf185fe35d42b962677c539d9
alejoescobar <alejoescobac@gmail.com>
2015-05-16 22:50:19 -0500

    Finish destroy command

--- lib/theroku/destroy_command.rb
+++ lib/theroku/destroy_command.rb
@@ -1,14 +1,21 @@
 class DestroyCommand
-  def exectute(args)
+  def execute(subdomain)
     if File.exists?('.theroku')
-      puts "Creating #{app_name}..."
+      puts "Deleting #{subdomain}..."
       file = File.open(".theroku", "rb")
       token = file.read
-      response = HTTParty.delete("#{Theroku::base_url}apps/#{app_name}",
-        body: { "url1" => "#{url1}", "url2" => "#{url2}", "app_name" => "#{app_name}" }.to_json,
+      response = HTTParty.delete("#{Theroku::base_url}apps/#{subdomain}",
+        body: { "subdomain" => "#{subdomain}" }.to_json,
         headers: { 'Authorization' => "Token token=#{token}", 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
       )
-      puts "Success!"
+      puts response
+      puts response.message
+      if response.code == 200
+        puts "You have successfully destroyed #{subdomain} app"
+      else
+        puts "There were the following errors:"
+        puts response["errors"].capitalize
+      end
     else
       puts "You have to login before making any changes you can do that with $ theroku login"
     end
